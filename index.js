6c352997e18e23ae93dc52ffd2ea4f0c3e59aedf
SAM CHRISTOPHER <samchristophergit07@gmail.com>
2024-08-31 16:48:03 +0530

    Added more Example

--- index.js
+++ index.js
@@ -7,11 +7,11 @@
 
 // hi().then((msg) => console.log(msg));
 
-let reach_A = new Promise((resolve, reject) => {
-  const reached = true;
-  if (reached) setTimeout(() => resolve("Mr.A Reached"), 3000);
-  else reject("Mr.A is not yet Reached");
-});
+// let reach_A = new Promise((resolve, reject) => {
+//   const reached = true;
+//   if (reached) setTimeout(() => resolve("Mr.A Reached"), 3000);
+//   else reject("Mr.A is not yet Reached");
+// });
 
 async function asyncstatus() {
   try {
@@ -25,3 +25,46 @@ async function asyncstatus() {
 }
 
 asyncstatus();
+
+//Tatkal function using async
+function tatkalBook(){
+  return new Promise((resolve,reject) => {
+      let bookingSuccess = true
+      if (bookingSuccess)
+          resolve(850)
+      else   
+          reject() 
+  })
+}
+
+//async function (function expression)
+const Booking_Deatail = async function (){
+  try{
+    first_Book = await tatkalBook();
+    console.log(first_Book);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+Booking_Deatail()
+
+// Another Example of using tosscoin function
+function tossCoin(){
+  return new Promise((resolve,reject)=>{
+      //0-head(success) 1 -tail(failure)
+      const rand = Math.floor(Math.random()*2)
+      if(rand==0)
+          resolve(1)
+      else
+          reject(0)
+  })
+}
+
+//async function (function declaration)
+
+async function CoinResult(){
+    result = await tossCoin()
+    console.log(result);
+}
+CoinResult()
